8f505baca9d3f6d2c17f01c042ea6a8e0c7bb802
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2018-03-23 12:17:45 +0100

    More testing.

--- test_threadedprocess.py
+++ test_threadedprocess.py
@@ -5,6 +5,7 @@ import time
 import pytest
 
 from threadedprocess import ThreadedProcessPoolExecutor
+from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
 
 
 def _get_pid_and_tid():
@@ -18,7 +19,20 @@ def _long_running_task():
     return (start, end)
 
 
+def _return_True():
+    return True
+
+
+def _raise_ValueError():
+    raise ValueError
+
+
 def test_futures_run_in_different_processes_and_threads():
+    """
+    Check that all spawned processes and threads are used to run the submitted
+    tasks.
+
+    """
     futures = []
 
     with ThreadedProcessPoolExecutor(max_processes=4,
@@ -31,9 +45,32 @@ def test_futures_run_in_different_processes_and_threads():
     assert len(pids_and_tids) == 16  # 4 processes, 4 threads
 
 
+def test_results_return():
+    with ThreadedProcessPoolExecutor() as executor:
+        f = executor.submit(_return_True)
+
+    assert f.result() is True
+
+
+def test_exceptions_raises():
+    with ThreadedProcessPoolExecutor() as executor:
+        f = executor.submit(_raise_ValueError)
+
+    with pytest.raises(ValueError):
+        assert f.result()
+
+
 @pytest.mark.wip
-def test_all_futures_execute_in_parallel_if_possible():
+@pytest.mark.parametrize("EXECUTOR", [
+    ProcessPoolExecutor(max_workers=16),
+    ThreadPoolExecutor(max_workers=16),
+    ThreadedProcessPoolExecutor(max_processes=4, max_threads=4)])
+def test_all_futures_execute_in_parallel_if_possible(EXECUTOR):
     """
+    If the number of tasks submitted to the executor is lower than the number
+    of workers the executor has, all the tasks should run concurrently (given
+    the task running time is not too short).
+
     0....1....2....3....4....5....6....7....8....9
     # DO OVERLAP
          A-----------------B
@@ -50,8 +87,7 @@ def test_all_futures_execute_in_parallel_if_possible():
     """
     futures = []
 
-    with ThreadedProcessPoolExecutor(max_processes=4,
-                                     max_threads=4) as executor:
+    with EXECUTOR as executor:
         for i in range(16):
             futures.append(executor.submit(_long_running_task))
 
