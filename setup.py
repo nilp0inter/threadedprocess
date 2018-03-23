f5720c7fe975beaf448e15ccd2613a6523d2662a
Roberto Abdelkader Martínez Pérez <robertomartinezp@gmail.com>
2018-03-23 16:17:57 +0100

    Bump version to change python versions in setup.py

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def read(fname):
 
 setup(
     name="threadedprocess",
-    version="0.0.1",
+    version="0.0.2",
     author="Roberto Abdelkader Martinez Perez",
     author_email="robertomartinezp@gmail.com",
     description=(
